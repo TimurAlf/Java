aef6f832c0af4cfcfa141e1924d92ff51a23cf15
Timur <47ta@bk.ru>
2023-08-15 15:30:19 +0500

    README.md NEW

---
+++ Lesson6.java
@@ -0,0 +1,11 @@
+package java_project;
+
+/**
+ * program
+ */
+public class Lesson6 {
+    public static void main(String[] args) {
+        System.out.println("arartata");
+        System.out.println("fdas");
+    }
+}
